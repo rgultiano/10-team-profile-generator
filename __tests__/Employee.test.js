4e42846f803f3890627eefb49822a3186898a579
Ron G <62861315+rgultiano@users.noreply.github.com>
2021-08-02 16:58:01 +1000

    Added Employee Test Cases

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,78 @@
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+      it("should create a object with a 'name' property set to the 'name' argument provided when called with the 'new' keyword", () => {
+        const e_name = 'test name';
+        const e_id = 'test1234abc';
+        const e_email = 'test@email.com';
+        
+        const obj = new Employee(e_name, e_id, e_email);
+
+        expect(obj.name).toEqual(e_name);
+      });
+  
+      it("should create a object with a 'id' property set to the 'id' argument provided when called with the 'new' keyword", () => {
+        const e_name = 'test name';
+        const e_id = 'test1234abc';
+        const e_email = 'test@email.com';
+        
+        const obj = new Employee(e_name, e_id, e_email);
+
+        expect(obj.id).toEqual(e_id);
+      });
+  
+      it("should create a object with a 'email' property set to the 'email' argument provided when called with the 'new' keyword", () => {
+        const e_name = 'test name';
+        const e_id = 'test1234abc';
+        const e_email = 'test@email.com';
+        
+        const obj = new Employee(e_name, e_id, e_email);
+
+        expect(obj.email).toEqual(e_email);
+      });
+
+    });
+
+     describe("Get Functions", () => {
+      it("getName() should return the employee name", () => {
+        const e_name = 'test name';
+        const e_id = 'test1234abc';
+        const e_email = 'test@email.com';
+        
+        const obj = new Employee(e_name, e_id, e_email);
+
+        expect(obj.getName()).toEqual(e_name);
+      });
+  
+      it("getId()() should return the employee Id", () => {
+        const e_name = 'test name';
+        const e_id = 'test1234abc';
+        const e_email = 'test@email.com';
+        
+        const obj = new Employee(e_name, e_id, e_email);
+
+        expect(obj.getId).toEqual(e_id);
+      });
+  
+      it("getEmail() should return the employee email", () => {
+        const e_name = 'test name';
+        const e_id = 'test1234abc';
+        const e_email = 'test@email.com';
+        
+        const obj = new Employee(e_name, e_id, e_email);
+
+        expect(obj.getEmail()).toEqual(e_email);
+      });
+
+      it("getRole() should return the 'Employee'", () => {
+        const e_name = 'test name';
+        const e_id = 'test1234abc';
+        const e_email = 'test@email.com';
+        
+        const obj = new Employee(e_name, e_id, e_email);
+
+        expect(obj.getRole()).toEqual('Employee');
+      });
+
+    });
+  });
